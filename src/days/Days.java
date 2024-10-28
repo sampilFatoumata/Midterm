c113a01d2e8e4de37388a060605f356a7191eb06
Fatoumata Sampil <sampil@sheridancollege.ca>
2024-10-28 18:26:02 -0400

    code2

--- src/days/Days.java
+++ src/days/Days.java
@@ -18,18 +18,45 @@ import java.util.Scanner;
  jn 22nd
  */
 public class Days {
+    
+
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+      
+       
          
-    Scanner in =new Scanner(System.in);
+   /* Scanner in =new Scanner(System.in);
     System.out.println("Enter the day number (one,two,three,four,five) in string");
     String code = in.next();
     Weekdays t= new Weekdays();
     t.nameOfDay(code); 
-    }// TODO code application logic here
+    */
+   
+  
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the day of the week (e.g., MONDAY): ");
+        String userInput = input.nextLine().toUpperCase();
+
+        // Convert user input to a Day enum
+        Day selectedDay;
+        try {
+            selectedDay = Day.valueOf(userInput);
+            Weekdays weekdays = new Weekdays();
+            weekdays.printDay(selectedDay);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid day entered. Please enter a valid day of the week.");
+ }
+
+
+     
+        }
+    }
+
+    // TODO code application logic here
     
     
-}
+
