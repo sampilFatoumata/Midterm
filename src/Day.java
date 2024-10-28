732061c1355d15abdde4cfb74956681394cd5de6
Fatoumata Sampil <sampil@sheridancollege.ca>
2024-10-28 18:26:36 -0400

    code3

---
+++ src/Day.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+
+/**
+ *
+ * @author Owner
+ */
+public enum Day {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY,SUNDAY}
+    
+
